3767de3492c3a0b31cf0f2d9b9571652cb759f68
Mine ALTUĞ <99974782+mialtgg@users.noreply.github.com>
2024-08-06 14:14:45 +0300

    version1

--- user/urls.py
+++ user/urls.py
@@ -3,4 +3,11 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('coins', views.coins, name='coins'),
+    path('coins/', views.coins, name='coins'),
+    path('seals/', views.seals, name='seals'),
+    path('seal-issuer/', views.seal_issuer, name='seal_issuer'),
+    path('sources/', views.sources, name='sources'),
+    path('mints/', views.mints, name='mints'),
+    path('all-datas/', views.all_datas, name='all_datas'),
 ]
--- user/views.py
+++ user/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'numismatics/home.html')
+def coins(request):
+    return render(request,'numismatics/coins.html')
+def seals(request):
+    return render(request, 'numismatics/seals.html')
+
+def seal_issuer(request):
+    return render(request, 'numismatics/seal_issuer.html')
+
+def sources(request):
+    return render(request, 'numismatics/sources.html')
+
+def mints(request):
+    return render(request, 'mints.html')
+
+def all_datas(request):
+    return render(request, 'numismatics/all_datas.html')
